8fcd4d2df1dcee4dd1a00ba6e8a5722de8e2c58a
Maksim Shevchuk <maksim.shauchuk@ovoko.com>
2021-06-29 19:22:48 +0300

    Add book parser

---
+++ book.php
@@ -0,0 +1,28 @@
+<?php
+require('phpQuery/phpQuery.php');
+
+$id = 83104;
+$url = "http://loveread.ec/read_book.php?id=%d&p=%d";
+$page = 1;
+$endPage = 86;
+$content = [];
+
+function parsePage($id, $page, $endPage, $url, &$content)
+{
+    $pageUrl = sprintf($url, $id, $page);
+    $text = file_get_contents($pageUrl);
+    $doc = phpQuery::newDocumentHTML($text);
+    foreach ($doc->find('div.MsoNormal') as $div)
+    {
+        $content[] = trim(str_replace('Страница', '', $div->textContent));
+    }
+
+    if ($page !== $endPage) {
+        $page++;
+        parsePage($id, $page, $endPage, $url, $content);
+    }
+}
+
+parsePage($id, $page, $endPage, $url, $content);
+
+file_put_contents($id . '.txt', implode(PHP_EOL, $content));
